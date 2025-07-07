560b50afbd331fad96391d942d9b7040b77dcae5
MizanRbf <mizanrbf@gmail.com>
2025-07-07 20:57:47 +0600

    react-router installed and created router for routing all pages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#bb0101',
+        secondary: '#260000',
+      }
+    },
+  },
+  plugins: [],
+}

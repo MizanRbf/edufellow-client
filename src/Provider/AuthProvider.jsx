0394d3d22f3215e950f7cd43943c3c2f45ffb4b5
MizanRbf <mizanrbf@gmail.com>
2025-07-08 08:33:44 +0600

    authProvider created to make easyer access authinfo from anywhere

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../Firebase/Firebase.config";
+import { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import axios from "axios";
 
-const AuthProvider = () => {
-  return <div></div>;
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState();
+  // Create User
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Update User
+  const updateUser = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  // Login User
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //  GoogleLogin
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  // Logout User
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // OnAuthStateChange
+  useEffect(() => {
+    const unSubsCribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+
+      // Token Generate
+
+      if (currentUser?.email) {
+        const userInfo = { email: currentUser.email };
+        axios
+          .post("https://langveta-server.vercel.app/jwt", userInfo, {
+            withCredentials: true,
+          })
+          .then()
+          .catch((error) => console.log(error));
+      }
+
+      // console.log("user in the auth state change", currentUser);
+    });
+    return () => {
+      unSubsCribe();
+    };
+  }, []);
+
+  const authInfo = {
+    createUser,
+    updateUser,
+    loginUser,
+    googleLogin,
+    logOutUser,
+    user,
+    setUser,
+    loading,
+    setLoading,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
-
 export default AuthProvider;

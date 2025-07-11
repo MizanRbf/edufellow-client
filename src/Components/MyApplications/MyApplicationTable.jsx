715744e6e554c75aab6ebff2b5c5390369e68305
MizanRbf <mizanrbf@gmail.com>
2025-07-11 23:11:43 +0600

    myApplication page creatd and userApplication show fetching from db

---
+++ src/Components/MyApplications/MyApplicationTable.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const MyApplicationTable = ({ application, index }) => {
+  const { university_name, scholarship_category, subject_category, photo } =
+    application;
+  return (
+    <tr className="border-2 border-slate-200">
+      <th>{index + 1}</th>
+      <td>
+        <img
+          className="rounded-xl md:max-w-20 md:h-20 min-w-30 h-30 border-2 p-1 border-primary"
+          src={photo}
+          alt=""
+        />
+      </td>
+      <td>{university_name}</td>
+      <td>{scholarship_category}</td>
+      <td>{subject_category}</td>
+      <td>
+        <div className="flex items-center gap-3">
+          {/* Update Button */}
+          {/* <Link to={`/updateMyTutorials/${myTutorials._id}`}>
+                    <button className="bg-blue-700 p-2 rounded-sm text-white btn border-0">
+                      <MdEdit className="text-xl" />
+                    </button>
+                  </Link> */}
+          {/* Delete Button */}
+          {/* <Link>
+                    <button
+                      onClick={() => handleDelete(myTutorials._id)}
+                      className="bg-red-500 p-2 rounded-sm text-white btn border-0"
+                    >
+                      <MdDelete className="text-xl" />
+                    </button>
+                  </Link> */}
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default MyApplicationTable;
--- src/Pages/Dashboard/MyApplication.jsx
+++ src/Pages/Dashboard/MyApplication.jsx
@@ -1,9 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useAuth from "../../Hooks/useAuth";
+import Loader from "../../Shared/Loader";
+import MyApplicationTable from "../../Components/MyApplications/MyApplicationTable";
 
 const MyApplication = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  // Get My Application
+  const email = user.email;
+  const {
+    data: myApplications,
+    isLoading,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ["myApplications"],
+    queryFn: async () => {
+      const res = await axiosSecure("/applicants", { email });
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loader></Loader>;
+  }
+  if (isError) {
+    console.log(error.message);
+  }
+
   return (
     <div>
-      <h1>MyApplication</h1>
+      <h1>My Applications</h1>
+      <table className="table">
+        {/* head */}
+
+        <thead className={`text-lg ${myApplications.length < 1 && "hidden"}`}>
+          <tr className="text-primary">
+            <th>No.</th>
+            <th>Image</th>
+            <th>University Name</th>
+            <th>Scholarship Category</th>
+            <th>Subject Category</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {myApplications.map((application, index) => (
+            <MyApplicationTable
+              key={application._id}
+              application={application}
+              index={index}
+            ></MyApplicationTable>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };

73c199d24a2e2930acf09d1181135e5a8792edad
MizanRbf <mizanrbf@gmail.com>
2025-07-12 00:10:40 +0600

    modal added in myApplicationTable to take review from users

--- src/Components/MyApplications/MyApplicationTable.jsx
+++ src/Components/MyApplications/MyApplicationTable.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const MyApplicationTable = ({ application, index }) => {
   const { university_name, scholarship_category, subject_category, photo } =
     application;
+
   return (
     <tr className="border-2 border-slate-200">
       <th>{index + 1}</th>
@@ -33,6 +34,30 @@ const MyApplicationTable = ({ application, index }) => {
                       <MdDelete className="text-xl" />
                     </button>
                   </Link> */}
+
+          {/* Review Modal */}
+
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="btn"
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+          >
+            Add Review
+          </button>
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
       </td>
     </tr>

6bff6025f6bf57ffd1a6565c569104aa02c1cafa
MizanRbf <mizanrbf@gmail.com>
2025-07-15 00:34:39 +0600

    user delete button added in manage users for admin

--- src/Components/ManageUsers/UserCard.jsx
+++ src/Components/ManageUsers/UserCard.jsx
@@ -1,7 +1,47 @@
+import { useQueryClient } from "@tanstack/react-query";
 import React from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const UserCard = ({ user, index }) => {
   const { name, email, role, photo } = user;
+  const queryClient = useQueryClient();
+  const axiosSecure = useAxiosSecure();
+
+  // handleDelete
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .delete(`/user/${id}`)
+          .then((res) => {
+            if (res.data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "user deleted successfully!",
+                icon: "success",
+              });
+              // Update cache
+              queryClient.setQueryData(["users"], (oldData) =>
+                oldData?.filter((user) => user._id !== id)
+              );
+            }
+          })
+          .catch((err) => Swal.fire("Error", err.message, "error"));
+      }
+    });
+  };
+
   return (
     <tr className="border-2 border-slate-200">
       <th>{index + 1}</th>
@@ -17,21 +57,14 @@ const UserCard = ({ user, index }) => {
       <td>{role}</td>
       <td>
         <div className="flex items-center gap-3">
-          {/* Update Button */}
-          {/* <Link to={`/updateMyTutorials/${myTutorials._id}`}>
-                    <button className="bg-blue-700 p-2 rounded-sm text-white btn border-0">
-                      <MdEdit className="text-xl" />
-                    </button>
-                  </Link> */}
           {/* Delete Button */}
-          {/* <Link>
-                    <button
-                      onClick={() => handleDelete(myTutorials._id)}
-                      className="bg-red-500 p-2 rounded-sm text-white btn border-0"
-                    >
-                      <MdDelete className="text-xl" />
-                    </button>
-                  </Link> */}
+          <button
+            onClick={() => handleDelete(user?._id)}
+            className="btn btn-outline btn-error flex items-center gap-2 hover:text-white hover:bg-red-500"
+          >
+            <MdDelete size={20} />
+            Delete
+          </button>
         </div>
       </td>
     </tr>

5c55d0b082492b1bf3756685bfacdcb9e14a3a54
MizanRbf <mizanrbf@gmail.com>
2025-07-24 07:33:35 +0600

    why Choose us section updeted with annimation

--- src/Pages/Home/WhyChooseUs/WhyChooseUs.jsx
+++ src/Pages/Home/WhyChooseUs/WhyChooseUs.jsx
@@ -33,43 +33,76 @@ const features = [
   },
 ];
 
+import { motion } from "motion/react";
+const Section1 = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0, y: 50 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6, ease: "easeOut" }}
+    viewport={{ once: false, amount: 0.3 }}
+  >
+    {children}
+  </motion.div>
+);
+const Section2 = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0, x: 25 }}
+    whileInView={{ opacity: 1, x: 0 }}
+    transition={{ duration: 0.6, ease: "easeOut" }}
+    viewport={{ once: false, amount: 0.3 }}
+  >
+    {children}
+  </motion.div>
+);
+
 const WhyChooseUs = () => {
   return (
     <section id="why-choose-us">
-      <div className="flex justify-between gap-8 items-center bg-[#224f6d25] p-10 rounded-4xl flex-wrap md:flex-nowrap">
+      <div className="flex justify-between gap-8 items-center bg-[#224f6d25] p-10 rounded-sm flex-wrap md:flex-nowrap">
         {/* Left Side: Features (60%) */}
         <div className="w-full md:w-[60%]">
           <div className="space-y-4">
             {features.map((item, idx) => (
-              <div
-                key={idx}
-                className="flex items-start gap-4 p-4 rounded-xl shadow-sm bg-white hover:shadow-lg transition"
-              >
+              <Section1>
                 <div
-                  className={`w-12 h-12 flex items-center justify-center rounded-xl ${item.bg}`}
+                  key={idx}
+                  className="flex flex-col sm:flex-row items-start 
+                  sm:items-center gap-4 p-4 rounded-sm shadow-sm bg-white hover:shadow-lg transition text-center md:text-left"
                 >
-                  {item.icon}
+                  <div
+                    className="flex justify-center w-full md:w-12 
+                  h-12
+                  md:justify-left"
+                  >
+                    <div
+                      className={`w-12 h-12 flex items-center justify-center rounded-xl ${item.bg}`}
+                    >
+                      {item.icon}
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      {item.title}
+                    </h3>
+                    <p className="text-sm text-gray-600">{item.description}</p>
+                  </div>
                 </div>
-                <div>
-                  <h3 className="text-lg font-semibold text-gray-800">
-                    {item.title}
-                  </h3>
-                  <p className="text-sm text-gray-600">{item.description}</p>
-                </div>
-              </div>
+              </Section1>
             ))}
           </div>
         </div>
 
         {/* Right Side: Image (40%) */}
         <div className="w-full md:w-[40%] flex justify-center md:justify-end mt-8 md:mt-0">
-          <div className="rounded-tr-[100px] rounded-bl-[100px] overflow-hidden shadow-xl max-w-sm w-full">
-            <img
-              src={whyChooseUs}
-              alt="Scholarship support"
-              className="w-full h-auto max-h-[500px] object-contain"
-            />
-          </div>
+          <Section2>
+            <div className="rounded-tr-[100px] rounded-bl-[100px] overflow-hidden shadow-xl max-w-sm w-full">
+              <img
+                src={whyChooseUs}
+                alt="Scholarship support"
+                className="w-full h-auto max-h-[500px] object-contain"
+              />
+            </div>
+          </Section2>
         </div>
       </div>
     </section>

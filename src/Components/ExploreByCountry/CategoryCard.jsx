879ff427e71d06ccbba9a9e8cc1610c29629952d
MizanRbf <mizanrbf@gmail.com>
2025-07-25 00:53:57 +0600

    explore by country updated

--- src/Components/ExploreByCountry/CategoryCard.jsx
+++ src/Components/ExploreByCountry/CategoryCard.jsx
@@ -1,41 +1,20 @@
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
-import useAxiosSecure from "../../Hooks/useAxiosSecure";
-
 const CategoryCard = ({ country }) => {
-  const axiosSecure = useAxiosSecure();
-  const [countryCount, setCountryCount] = useState([]);
-  useEffect(() => {
-    const fetchTutors = async () => {
-      try {
-        const res = await axiosSecure(
-          `/scholarshipByCategory/${country?.country}`
-        );
-        const data = await res.data;
-        setCountryCount(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchTutors();
-  }, [country?.country, axiosSecure]);
-
   return (
     <Link to={`/scholarshipByCountry/${country.country}`}>
-      <div className="border border-primary p-4 rounded-sm flex gap-3 items-center">
+      <div className="p-4 rounded-full flex gap-10 items-center shadow-lg bg-white">
         {/* Image */}
-        <div>
-          <img className="shadow-lg" src={country.flag} alt="" />
+        <div className="w-24 h-24 rounded-full  overflow-hidden shadow-lg animate-spin-slow">
+          <img
+            src={country.flag}
+            alt="Flag"
+            className="w-full h-full object-cover"
+          />
         </div>
 
         {/* Rest Item */}
         <div>
-          <h3>{country.country}</h3>
-          <p>
-            <span className="font-bold">Scholarships: </span>{" "}
-            {countryCount?.length}
-          </p>
+          <h2 className="">{country.country}</h2>
         </div>
       </div>
     </Link>
